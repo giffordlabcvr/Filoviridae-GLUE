82d05b14dcc4fb665733978b9f6d0463e5b6ff7b
Robert Gifford <robjgiff@gmail.com>
2020-07-09 05:01:15 +0100

    Refactoring

--- glue/eve/loadRefConData2.js
+++ glue/eve/loadRefConData2.js
@@ -7,14 +7,12 @@ glue.inMode("module/filoviridaeTabularUtility", function() {
 
 _.each(loadResult, function(eveObj) {
 
-	glue.inMode("custom-table-row/refcon_data/"+eveObj.id, function() {
+	glue.inMode("custom-table-row/refcon_data/"+eveObj.sequenceID, function() {
 	
-		glue.log("INFO", "Entering locus data for EVE reference:", eveObj.id);
+		glue.log("INFO", "Entering locus data for EVE reference:", eveObj.sequenceID);
 
+		glue.command(["set", "field", "num_copies", eveObj.num_copies]);
 		glue.command(["set", "field", "locus_numeric_id", eveObj.locus_numeric_id]);
-		glue.command(["set", "field", "nearest_upstream_orf", eveObj.nearest_upstream_orf]);
-		glue.command(["set", "field", "nearest_downstream_orf", eveObj.nearest_downstream_orf]);
-		glue.command(["set", "field", "host_group_name", eveObj.host_group_name]);
 
 	});
 
